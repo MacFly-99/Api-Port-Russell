207aeb3d273678f2d7aee8e4841b1085b80a9253
MacFly <matteo.ventura1999@gmail.com>
2026-01-14 01:58:05 +0100

    API structure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const userRoute = require('../routes/user');
+
+router.get('/', async (req, res) => {
+    res.status(200).json({
+        name: process.env.APP_NAME,
+        version: '1.0',
+        status: 200,
+        message: 'Bienvenue sur l\'API du Port de Russell'
+    });
 });
+
+router.use('/users', userRoute);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const fichier = require('../fichiers/users');
+
+// Route pour lire les infos d'un utilisateur
+router.get('/:id', fichier.getById);
+
+// Route pour ajouter un nouvel utilisateur
+router.put('/add', fichier.add);
+
+// Route pour mettre à jour,modifier un utilisateur existant
+router.patch('/:id', fichier.update);
+
+// Route pour supprimer un utilisateur
+router.delete('/:id', fichier.delete);
 
 module.exports = router;
